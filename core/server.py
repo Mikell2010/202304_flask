1eb8a050642381137ebd6f6de363fc7423986426
Michel <mschocken@gmail.com>
2023-06-01 17:38:59 -0400

    contador

--- core/server.py
+++ core/server.py
@@ -1,21 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
+app.secret_key = 'Bunny bob was here'
 
-
-@app.route("/")
+@app.route('/')
 def index():
+    if 'count' not in session:
+        session['count'] =0
+    session['count'] +=1
+    return render_template("index.html")
 
-    # Data source
-    users = [
-        {'first_name' : 'Michael', 'last_name' : 'Choi'},
-        {'first_name' : 'John', 'last_name' : 'Supsupin'},
-        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-        {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
-    return render_template("index.html",users=users)
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True, port=5001) 
